6bebb5a3a741cca5f977125fbbca34ceef76ec15
Alex Popof <alex.popof@gmail.com>
2014-04-26 16:08:59 -0700

    reformatting and adding video example code

--- server.js
+++ server.js
@@ -4,17 +4,49 @@
 var http = require('http');
 var io = require('socket.io');
 var fs = require('fs');
+var url = require('url');
+var fs = require('fs');
 
 var server = http.createServer(function(req,rep){
   console.log('connection');
-  fs.readFile(__dirname+"/index.html", function(error,data){
-    if (error) {
-      console.log('error');
-    }
-    rep.writeHead(200, {'Content-Type':'text/html'});
-    rep.write(data, 'utf8');
-    rep.end();
-  });
+  var path = url.parse(req.url).pathname;
+  console.log(path);
+  switch(path) {
+    case '/':
+      fs.readFile(__dirname+"/index.html", function(error,data){
+        if (error) {
+          console.log('error');
+        }
+        rep.writeHead(200, {'Content-Type':'text/html'});
+        rep.write(data, 'utf8');
+        rep.end();
+      });
+      break;
+    case '/audio.html':
+      fs.readFile(__dirname+"/audio.html", function(error,data){
+        if (error) {
+          console.log('error');
+        }
+        rep.writeHead(200, {'Content-Type':'text/html'});
+        rep.write(data, 'utf8');
+        rep.end();
+      });
+      break;
+    case '/video.html':
+      fs.readFile(__dirname+"/video.html", function(error,data){
+        if (error) {
+          console.log('error');
+        }
+        rep.writeHead(200, {'Content-Type':'text/html'});
+        rep.write(data, 'utf8');
+        rep.end();
+      });
+      break;
+    default:
+      rep.writeHead(404);
+      rep.write("opps this doesn't exist - 404");
+      break;
+  }
 });
 
 server.listen(8080);

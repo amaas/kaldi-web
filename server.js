3871b43a2e7b706b47f8055e77f9f614160b1cb9
Alex Popof <alex.popof@gmail.com>
2014-05-26 12:57:13 -0700

    code cleanup, commenting

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ var server = http.createServer(function(req,rep){
   var path = url.parse(req.url).pathname;
   console.log(path);
   switch(path) {
+    // Capture audio from mic and send it back to server
     case '/':
       fs.readFile(__dirname+"/index.html", function(error,data){
         if (error) {
@@ -24,29 +25,8 @@ var server = http.createServer(function(req,rep){
         rep.end();
       });
       break;
-    // example of writing to a file using fs
-    case '/write':
-      fs.writeFile("./test.txt", "Hey there!", function(err) {
-        if(err) {
-            console.log(err);
-        } else {
-            console.log("The file was saved!");
-        }
-      });
-      rep.writeHead(200, {'Content-Type':'text/html'});
-      rep.write("<html><body>Writng</body></html>");
-      rep.end();
-      break;
-    case '/audio.html':
-      fs.readFile(__dirname+"/audio.html", function(error,data){
-        if (error) {
-          console.log('error');
-        }
-        rep.writeHead(200, {'Content-Type':'text/html'});
-        rep.write(data, 'utf8');
-        rep.end();
-      });
-      break;
+    // Working version where audio is captured, downsampled, and sent to server via socket
+    // Kaldi decodes and server sends result back to the html
     case '/new_index.html':
       fs.readFile(__dirname+"/new_index.html", function(error,data){
         if (error) {
@@ -57,16 +37,7 @@ var server = http.createServer(function(req,rep){
         rep.end();
       });
       break;
-    case '/video.html':
-      fs.readFile(__dirname+"/video.html", function(error,data){
-        if (error) {
-          console.log('error');
-        }
-        rep.writeHead(200, {'Content-Type':'text/html'});
-        rep.write(data, 'utf8');
-        rep.end();
-      });
-      break;
+    // Javascript files used with new_index.html
     case '/recorder.js':
       fs.readFile(__dirname+"/recorder.js", function(error,data){
         if (error) {
@@ -97,7 +68,11 @@ var server = http.createServer(function(req,rep){
         rep.end();
       });
       break;
-    // sample invocations of command line process (wc)
+
+    // END OF ACTIVE CODE
+    // START DEMO ROUTES
+
+    // sample invocations of command line process
     // http://stackoverflow.com/questions/20643470/execute-a-command-line-binary-in-node-js
     // execute_all: fetches complete output
     // execute_online: receives output as streams
@@ -125,6 +100,44 @@ var server = http.createServer(function(req,rep){
       rep.write("<html><body>Executing command ... fetch in chunks</body></html>");
       rep.end();
       break;
+    // example of writing to a file using fs
+    case '/write':
+      fs.writeFile("./test.txt", "Hey there!", function(err) {
+        if(err) {
+            console.log(err);
+        } else {
+            console.log("The file was saved!");
+        }
+      });
+      rep.writeHead(200, {'Content-Type':'text/html'});
+      rep.write("<html><body>Writng</body></html>");
+      rep.end();
+      break;
+
+    // This file was part of the starter code but abandoned in favor of
+    // new_index.html
+    case '/audio.html':
+      fs.readFile(__dirname+"/audio.html", function(error,data){
+        if (error) {
+          console.log('error');
+        }
+        rep.writeHead(200, {'Content-Type':'text/html'});
+        rep.write(data, 'utf8');
+        rep.end();
+      });
+      break;
+    // An visual example of using user media with video 
+    case '/video.html':
+      fs.readFile(__dirname+"/video.html", function(error,data){
+        if (error) {
+          console.log('error');
+        }
+        rep.writeHead(200, {'Content-Type':'text/html'});
+        rep.write(data, 'utf8');
+        rep.end();
+      });
+      break;
+    // END DEMO ROUTES
     default:
       console.log("default path");
       rep.writeHead(404);
@@ -141,19 +154,21 @@ io = io.listen(server);
 io.set('log level', 1);
 
 io.sockets.on('connection',function (socket) {
+  // used in index.html
   socket.on('left', function (data) {
     console.log(data.left);
   });
+  //used in new_index.html
   socket.on('wav', function (data) {
     fs.writeFile('test.wav', data.str, 'binary');
     // run the kaldi decode script
     var child = spawn('bash', ['../example.sh']);
-      child.stdout.on('data', function(chunk) {
-        // need to parse buffer data bytes into ascii
-        var returnedText = chunk.toString();
-        console.log(returnedText);
-        // send back to html file to display for user
-	socket.emit("decode", {'result': returnedText});
+    child.stdout.on('data', function(chunk) {
+      // need to parse buffer data bytes into ascii
+      var returnedText = chunk.toString();
+      console.log(returnedText);
+      // send back to html file to display for user
+	    socket.emit("decode", {'result': returnedText});
     });
   });
 });
